2a4fe8329f1b23659f16382e0765ec0db40ee8c6
Christian Loos <cloos@netsandbox.de>
2010-06-03 05:21:21 +0800

    using RT internal preferences system
    The activation and deactivation of this extension is moved from the
    user 'About me' page to the 'Settings' page because we are now using
    the RT internal preferences system.

--- lib/RT/Extension/SideBySideView/CustomView.pm
+++ lib/RT/Extension/SideBySideView/CustomView.pm
@@ -2,4 +2,14 @@ package RT::Extension::SideBySideView::CustomView;
 
 our $VERSION = '0.02';
 
+$RT::Config::META{'SideBySideView'} = {
+    Section         => 'RT::Extension::SideBySideView',
+    Overridable     => 1,
+    SortOrder       => 1,
+    Widget          => '/Widgets/Form/Boolean',
+    WidgetArguments => {
+        Description => 'SideBySideView', # loc
+    },
+};
+
 1;
